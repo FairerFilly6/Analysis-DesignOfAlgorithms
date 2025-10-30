a6cf2d623823a41d39ddcdfed85c8e6e540dacd2
OmarBarron <omar@administro.mx>
2025-10-29 19:29:28 -0600

    Comentarios

--- Practica4/distanciasEuclidianas.py
+++ Practica4/distanciasEuclidianas.py
@@ -1,4 +1,7 @@
 import math
+import tracemalloc
+import time
+import random
 
 def distancia(p1, p2):
     return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
@@ -6,7 +9,7 @@ def distancia(p1, p2):
 def par_mas_cercano(puntos):
     n = len(puntos)
     
-    # Caso base directo dentro del divide and conquer
+    #Caso base 
     if n <= 3:
         min_d = float('inf')
         par = (None, None)
@@ -18,17 +21,17 @@ def par_mas_cercano(puntos):
                     par = (puntos[i], puntos[j])
         return par[0], par[1], min_d
 
-    #  Dividir por la mitad
+    #split
     mid = n // 2
     mitad_x = puntos[mid][0]
     izq = puntos[:mid]
     der = puntos[mid:]
 
-    #  Resolver recursivamente
+   
     p1_izq, p2_izq, d_izq = par_mas_cercano(izq)
     p1_der, p2_der, d_der = par_mas_cercano(der)
 
-    #  Obtener el mínimo actual
+
     if d_izq < d_der:
         d_min = d_izq
         par_min = (p1_izq, p2_izq)
@@ -54,9 +57,29 @@ def par_mas_cercano(puntos):
     return par_min[0], par_min[1], d_min
 
 
-# Ejemplo de uso
-puntos = [(2,3), (12,30), (40,50), (5,1), (12,10), (3,4)]
-puntos.sort(key=lambda p: p[0])  # importante: ordenar por coordenada X
+
+cantPuntos = int(input("Ingresa el numero de puntos que deseas: ")) 
+
+tracemalloc.start()
+start = time.time()
+
+puntos = [(random.randint(0, 100), random.randint(0, 100)) for _ in range(cantPuntos)]
+
+print(puntos)
+
+puntos.sort(key=lambda p: p[0])  
+
+
+
 
 p1, p2, dist = par_mas_cercano(puntos)
 print(f"Los puntos más cercanos son {p1} y {p2} con distancia {dist:.3f}")
+
+
+end = time.time()
+current, peak = tracemalloc.get_traced_memory()
+tracemalloc.stop()
+print(f" Memoria actual: {current / 1024} KB; Pico: {peak / 1024} KB")
+memoria = (peak / 1024) - (current / 1024)
+print(f"Consumo de memoria: {memoria} ")
+print(f" Tiempo de ejecución: {end - start} segundos\n")
