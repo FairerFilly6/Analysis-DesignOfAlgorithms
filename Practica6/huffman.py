4d10be608d2aea69a84b2b2795967af8a771f0f6
OmarBarron <omar@administro.mx>
2025-11-30 20:14:21 -0600

    Generacion del arbol

--- Practica6/huffman.py
+++ Practica6/huffman.py
@@ -1,5 +1,5 @@
 
-# text = input("Ingrese una cadena: ")
+import heapq
 
 text = """Omar
 Andrés
@@ -18,17 +18,90 @@ for caracter in text:
     else:
         frecuencias[caracter] = 1
 
-print(frecuencias)
+# print(frecuencias)
 
 frecuencias = sorted(frecuencias.items(), key=lambda x: x[1], reverse=True)
 
-print(frecuencias)
+# print(frecuencias)
 print("frecuencias usadas")
 for frecuencia in frecuencias:
     print(frecuencia)
 
+nodos = []
+
+for elemento in frecuencias:
+    nodo = {
+        "caracter": elemento[0],
+        "frecuencia": elemento[1],
+        "izq": None,
+        "der": None
+    }
+    nodos.append(nodo)
+
+for nodo in nodos:
+    print(nodo)
+
+heap = []
+contador = 0
+
+for nodo in nodos:
+    heap.append((nodo['frecuencia'], contador, nodo))
+    contador += 1
+
+heapq.heapify(heap)
+
+print("Heap")
+
+for elemento in heap:
+    print(elemento)
+
+print("algoritmo")
+
+while(len(heap) > 1):
+    freq1, _, nodoA = heapq.heappop(heap)
+    freq2, _, nodoB = heapq.heappop(heap)
+
+    # print(f"nodoA {nodoA} ")
+    # print(f"nodoB {nodoB}")
+
+    
+    freqTotal = freq1 + freq2
+
+    nuevo = {
+        'caracter': None,
+        'frecuencia': freqTotal,
+        'izq': nodoA,
+        'der': nodoB
+    }
+    
+    heapq.heappush(heap,(freqTotal, contador, nuevo))
+    contador+=1
 
 
+print(heap)
+arbol = heap[0][2]
+print("arbol")
+
+print(arbol)
+
+def mostrarArbol(nodo, nivel=0):
+    if nodo is None:
+        return
+
+    # sangría según profundidad
+    indent = "    " * nivel
+
+    if nodo['caracter'] is None:
+        print(f"{indent}Nodo interno ({nodo['frecuencia']})")
+    else:
+        print(f"{indent}'{nodo['caracter']}' ({nodo['frecuencia']})")
+
+    mostrarArbol(nodo['izq'], nivel + 1)
+    mostrarArbol(nodo['der'], nivel + 1)
+
+
+mostrarArbol(arbol)
+    
 
 
 
